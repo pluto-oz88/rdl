68412e24afbfbb4d0eb12be4316265376e059d16
Pluto <167601668+pluto-oz88@users.noreply.github.com>
2026-08-28 22:26:56 +1000

    Add local Google Places config template

---
+++ config.example.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    // Copy this file to config.local.php and paste the key for the RDL web-service environment.
+    'google_places_api_key' => 'PASTE_RDL_GOOGLE_PLACES_API_KEY_HERE',
+];
